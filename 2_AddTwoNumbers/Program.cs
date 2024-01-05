840bbb34ddba5bc9c0fa400ac2687007d6aef376
Tom Serrante <tserrante@gmail.com>
2024-01-05 15:51:54 -0500

    2_AddTwoNumbers partially solved without recursion

--- 2_AddTwoNumbers/Program.cs
+++ 2_AddTwoNumbers/Program.cs
@@ -1,28 +1,39 @@
-﻿using System.Collections.Generic;
-
-namespace AddTwoNumbers;
+﻿namespace AddTwoNumbers;
 
 public class Program
 {
     static void Main()
     {
+        LinkedList<int> l1 = new LinkedList<int>(new int[] {9,9,9,9,9,9,9});
+        LinkedList<int> l2 = new LinkedList<int>(new int[] {9,9,9,9});
 
+        LinkedList<int> sumOfL1L2 = AddTwoNumbers(l1.First, l2.First);
+        
+        System.Console.WriteLine(sumOfL1L2);
+        //LinkedListNode<int> currentNode = sumOfL1L2.First;
+
+        // while(currentNode != null)
+        // {
+        //     Console.WriteLine(currentNode.Value);
+        //     currentNode = currentNode.Next;
+        // }        
     }
     
     /**
         l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-             
+       11111111
         9999999
            9999
-              8          
+       10009998          
     */
 
-    public LinkedListNode<int> AddTwoNumbers(LinkedListNode<int> l1, LinkedListNode<int> l2)
+    public static LinkedList<int> AddTwoNumbers(LinkedListNode<int> l1, LinkedListNode<int> l2)
     {
         int l1AsInt = default;
         int l2AsInt = default;
+
         int count = 1;
-        while(l1.Next != null )
+        while(l1 != null )
         {
             l1AsInt += l1.Value * count;
             count *= 10; 
@@ -30,7 +41,7 @@ public class Program
         }
 
         count = 1;
-        while(l2.Next != null )
+        while(l2 != null )
         {
             l2AsInt += l2.Value * count;
             count *= 10; 
@@ -38,9 +49,25 @@ public class Program
         }
         
         int sumOfLists = l1AsInt + l2AsInt;
-
+        string sumAsString = sumOfLists.ToString();
+        
+        System.Console.WriteLine("Sum of Lists: {0}", sumAsString);
+        System.Console.WriteLine("Length of sum: {0}", sumAsString.Length);
         LinkedList<int> returnedList = new LinkedList<int>();
         
+        for(int i = 0; i < sumAsString.Length; i++)
+        {
+            // int num = Convert.ToInt32(sumOfLists / Math.Pow(10, i));
+            // Console.WriteLine("power: {0}\tval: {1}", i, num);
+            // returnedList.AddLast(new LinkedListNode<int>(num));
+            //(sumOfLists / Math.Pow(10, i));
+            returnedList.AddFirst(int.Parse(sumAsString[i].ToString()));
+        }
+        return returnedList;
+    }
 
+    public static LinkedListNode<int> AddTwoNumbers(LinkedListNode<int> l1, LinkedListNode<int> l2)
+    {
+        
     }
 }
