f8a54c291f3edddbb2637e7f87b162cb874e5068
Tom Serrante <tserrante@gmail.com>
2024-05-02 23:19:58 -0400

    hash table solution problem 3

--- 3_LengthOfLongestSubstring/Program.cs
+++ 3_LengthOfLongestSubstring/Program.cs
@@ -1,11 +1,20 @@
-﻿// Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
-// Console.WriteLine(LengthOfLongestSubstring("bbbbbb"));
-// Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
-Console.WriteLine(LengthOfLongestSubstring("au"));
+﻿using System.Collections.Generic;
+// Console.WriteLine("testing: " + "abcabcbb: --> " + Set_LengthOfLongestSubstring("abcabcbb"));
+// Console.WriteLine("testing: " + "bbbbbb: --> " + Set_LengthOfLongestSubstring("bbbbbb"));
+Console.WriteLine("testing: " + "pwwkew: --> " + Set_LengthOfLongestSubstring("pwwkew"));
+// Console.WriteLine("testing: " + "au: --> " + Set_LengthOfLongestSubstring("au"));
+// Console.WriteLine("testing: " + "' ': --> " + Set_LengthOfLongestSubstring(" "));
 
-int LengthOfLongestSubstring(string s) 
+int FirstSubmission_LengthOfLongestSubstring(string s) 
 {
+
+    if(s.Length <= 1)
+    {
+        return s.Length;
+    }
+
     int returnLength = 0;
+
     for(int i = 0; i < s.Length - 1; i++)
     {
         string substring = s[i].ToString();
@@ -27,4 +36,33 @@ int LengthOfLongestSubstring(string s)
         }
     }
     return returnLength;
+}
+
+int Set_LengthOfLongestSubstring(string s)
+{
+    HashSet<char> substring = new HashSet<char>();
+
+    bool isAdded = false;
+    int leftIndex = 0; 
+    int rightIndex = 0;
+    int maxLength = 0;
+
+    while(rightIndex < s.Length && leftIndex < s.Length)
+    {
+        isAdded = substring.Add(s[rightIndex]);
+
+        if(isAdded)
+        {
+            rightIndex++;
+            if(substring.Count > maxLength)
+                maxLength = substring.Count;
+        }
+        else
+        {
+            substring.Remove(s[leftIndex]);
+            leftIndex++;
+        }
+
+    }
+    return maxLength;
 }
