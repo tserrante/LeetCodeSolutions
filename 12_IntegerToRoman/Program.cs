1cc3fda654287f3ca7fe7cb2ef01ecf77e93b138
Tom S <tserrante@gmail.com>
2024-11-29 15:12:30 -0500

    sync

--- 12_IntegerToRoman/Program.cs
+++ 12_IntegerToRoman/Program.cs
@@ -9,6 +9,14 @@ public class Solution
 
     public string IntToRoman(int num)
     {
+        Dictionary<int, char> IntToString = new Dictionary<int, char>{
+            {1, 'I'}, {5, 'V'}, {10, 'X'}, {50, 'L'}, {100, 'C'}, {500, 'D'}, {1000, 'M'}
+        };
 
+        while(num > 0)
+        {
+             
+
+        }
     }
 }
