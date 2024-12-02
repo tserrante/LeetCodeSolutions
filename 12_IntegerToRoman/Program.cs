d362fae9ce90aae69570cf92f1b28e7ff49367a6
Tom S <tserrante@gmail.com>
2024-12-01 21:06:03 -0500

    12_IntToRoman solution

--- 12_IntegerToRoman/Program.cs
+++ 12_IntegerToRoman/Program.cs
@@ -1,22 +1,68 @@
-﻿namespace IntegerToRoman;
+﻿using System.Text;
 
-public class Solution
+namespace IntegerToRoman;
+
+public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-
+        Console.WriteLine("1990 - {0}", IntToRomanDict(1990));
+        Console.WriteLine("3994 - {0}", IntToRoman(3994));
+        Console.WriteLine("50 - {0}", IntToRoman(50));
     }
-
-    public string IntToRoman(int num)
+    public static string IntToRomanDict(int num)
     {
-        Dictionary<int, char> IntToString = new Dictionary<int, char>{
-            {1, 'I'}, {5, 'V'}, {10, 'X'}, {50, 'L'}, {100, 'C'}, {500, 'D'}, {1000, 'M'}
+        var romanNumberBuilder = new StringBuilder();
+        var romansMap = new Dictionary<int, string> 
+        {
+            { 1000, "M" }, { 900, "CM" }, { 500, "D" },
+            { 400, "CD" }, { 100, "C" }, { 90, "XC" },
+            { 50, "L" }, { 40, "XL" },{ 10, "X" },
+            { 9, "IX" }, { 5, "V" }, { 4, "IV" },
+            { 1, "I" }
         };
 
-        while(num > 0)
+        foreach(var romanInt in romansMap.Keys)
         {
-             
-
+            while(romanInt <= num)
+            {
+                romanNumberBuilder.Append(romansMap[romanInt]);
+                num -= romanInt;
+            }
         }
+
+        var romanNumber = romanNumberBuilder.ToString();
+        return romanNumber;
+    }
+
+    public static string IntToRoman(int num)
+    {
+        if (num >= 1000) return "M" + IntToRoman(num - 1000);
+
+        if (num >= 900) return "CM" + IntToRoman(num - 900);
+
+        if (num >= 500) return "D" + IntToRoman(num - 500);
+
+        if (num >= 400) return "CD" + IntToRoman(num - 400);
+
+        if (num >= 100) return "C" + IntToRoman(num - 100);
+
+        if (num >= 90) return "XC" + IntToRoman(num - 90);
+
+        if (num >= 50) return "L" + IntToRoman(num - 50);
+
+        if (num >= 40) return "XL" + IntToRoman(num - 40);
+
+        if (num >= 10) return "X" + IntToRoman(num - 10);
+
+        if (num >= 9) return "IX" + IntToRoman(num - 9);
+
+        if (num >= 5) return "V" + IntToRoman(num - 5);
+
+        if (num >= 4) return "IV" + IntToRoman(num - 4);
+
+        if (num >= 1) return "I" + IntToRoman(num - 1);
+
+        return "";
     }
 }
--- 12_IntegerToRoman/readme.md
+++ 12_IntegerToRoman/readme.md
@@ -1,13 +1,13 @@
 Seven different symbols represent Roman numerals with the following values:
 
 Symbol	Value
-I	    1
-V	    5
-X	    10
-L	    50
-C	    100
-D	    500
-M	    1000
+I	      1
+V	      5
+X	      10
+L	      50
+C	      100
+D	      500
+M	      1000
 Roman numerals are formed by appending the conversions of decimal place values from highest to lowest. Converting a decimal place value into a Roman numeral has the following rules:
 
 -If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
