ce6c6549e066809651770e1496671db1cbef9944
Tom S <tserrante@gmail.com>
2024-12-11 14:01:39 -0500

    21_MergeTwoSortedLists recursive solution

--- 21_MergeTwoSortedLists/Program.cs
+++ 21_MergeTwoSortedLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection.Metadata.Ecma335;
+
 namespace MergeTwoSortedLists;
 
 public class Program
@@ -34,5 +36,22 @@ public class Program
             current.next = list2;
 
         return head.next;
+    }
+
+    public static ListNode MergeTwoLists_Recursive(ListNode list1, ListNode list2)
+    {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        if(list1.val <= list2.val)
+        {
+            list1.next = MergeTwoLists_Recursive(list1.next, list2);
+            return list1;
+        }
+        else
+        {
+            list2.next = MergeTwoLists_Recursive(list1, list2.next);
+            return list2;
+        }
     }
 }
