0f1e5552b0a9ce8b3b96c31a06151ec91d510468
Tom S <tserrante@gmail.com>
2025-01-08 19:22:29 -0500

    5_LongestPalindromicSubstring init

---
+++ 5_LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace LongestPalindromicSubstring;
+
+public class Program
+{
+    public static void Main()
+    {
+        string test1 = "babad";
+        string test2 = "cbbd";
+    }
+
+    public static string LongestPalindrome(string s) {
+        string LongestPalindrome = string.Empty;
+
+        return LongestPalindrome;
+    }
+}
